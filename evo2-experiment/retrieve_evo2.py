4e6482075f5608cdc76ab23411f9ce674e2fbc08
michael ho <michaelho0307@gmail.com>
2025-07-17 08:55:11 +0000

    revise retrieve_evo2.py - adjust preprocess procedure

--- evo2-experiment/retrieve_evo2.py
+++ evo2-experiment/retrieve_evo2.py
@@ -1,4 +1,5 @@
 import os
+import gc
 import torch
 from evo2 import Evo2
 from tqdm import tqdm
@@ -7,37 +8,39 @@ CHECKPOINT_PATH = "checkpoint.pt"
 CHECKPOINT_EVERY = 10
 
 device = 'cuda:0'
-evo2_model = Evo2('evo2_7b')
-layer_name = 'blocks.28.mlp.l3'
+evo2_model = Evo2('evo2_1b_base') # evo2_7b
+layer_name = 'blocks.24.mlp.l3' # blocks.28.mlp.l3 (24: 1b_base)
 
-def retrieve_gene(base_dir: str):
+def retrieve_total_file_list(base_dir: str):
     # check every subdirectory in the base directory
-    samples = []
-    '''
-    sample = {
-        "disease": "disease_name" (Prediabetic_State),
-        "gene": "gene_sequence" (ATCGATCG...),
-        "ID": "sample_id" (SRR8534048)}
-    '''
+    file_list = []
     for root, dirs, files in os.walk(base_dir):
         for file in files:
             if file.endswith(".txt"):
-                gene_file_path = os.path.join(root, file)
-                with open(gene_file_path, 'r') as f:
-                    gene_data = f.read()
-                    sample = {
-                        "disease": os.path.basename(root),
-                        "gene": gene_data.strip(),
-                        "ID": os.path.splitext(file)[0]
-                    }
-                samples.append(sample)
-    return samples
-                    
-def slice_sequence(seq: str, window_size=100_000, overlap=50_000):
+                file_path = os.path.join(root, file)
+                file_list.append(file_path)
+    return file_list
+
+def retrieve_gene(filepath: str):
+    with open(filepath, 'r') as f:
+        gene_data = f.read().strip()
+    sample_id = os.path.splitext(os.path.basename(filepath))[0]
+    disease = os.path.basename(os.path.dirname(filepath))  # e.g., 'Prediabetic_State'
+    sample = {
+        "disease": disease,
+        "gene": gene_data,
+        "ID": sample_id
+    }
+    return sample
+    
+
+# 100000 & 50000 too large, GPU cannot handle                    
+def slice_sequence(seq: str, window_size=10000, overlap=5000):
     step = window_size - overlap
     slices = [seq[i:i+window_size] for i in range(0, len(seq) - window_size + 1, step)]
     return slices
 
+# TODO: make sure the shape is correct
 @torch.no_grad()
 def get_embedding(sequence: str):
     input_ids = torch.tensor(
@@ -46,20 +49,26 @@ def get_embedding(sequence: str):
     ).unsqueeze(0).to(device)
     
     _, embeddings = evo2_model(input_ids, return_embeddings=True, layer_names=[layer_name])
-    return embeddings[layer_name].squeeze(0)  # shape: [seq_len, hidden_dim]
+    return embeddings[layer_name].squeeze(0).cpu()  # shape: [seq_len, hidden_dim]
 
+# TODO: make sure the shape is correct
 def encode_sample(sample):
     gene_seq = sample["gene"]
     slices = slice_sequence(gene_seq)
-    embeddings = [get_embedding(s) for s in slices]
-    
-    # e.g., simple mean pooling over tokens and slices
-    pooled_embeddings = [e.mean(dim=0) for e in embeddings]  # mean over seq_len
-    final_embedding = torch.stack(pooled_embeddings).mean(dim=0)  # mean over slices
+    sum_embeddings = []
+
+    # for s in slices:
+    for s in tqdm(slices, desc=f"Processing {sample['ID']}"):
+        mean_embedding = get_embedding(s).mean(dim=0).cpu()
+        sum_embeddings.append(mean_embedding)
+        torch.cuda.empty_cache()
+        gc.collect()
+
+    final_embedding = torch.stack(sum_embeddings).mean(dim=0)  # mean over slices
     return final_embedding  # shape: [hidden_dim]
 
 if __name__ == "__main__":
-    samples = retrieve_gene("../fastq-experiment/gene_txt_folder")
+    genefiles = retrieve_total_file_list("../fastq-experiment/gene_txt_folder")
 
     if os.path.exists(CHECKPOINT_PATH):
         data = torch.load(CHECKPOINT_PATH)
@@ -73,7 +82,9 @@ if __name__ == "__main__":
 
     buffer_emb, buffer_label, buffer_id = [], [], []
 
-    for sample in tqdm(samples):
+    for genefile in tqdm(genefiles):
+        sample = retrieve_gene(genefile)
+
         if sample["ID"] in ids:
             continue  
 
